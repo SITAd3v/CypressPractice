bb4538b4668db23dd15ce3558603113d055ae4ec
Pooja-Gangavathi <pujagv@gmail.com>
2023-12-15 12:05:06 +0100

    Added some changes

---
+++ cypress/e2e/Spec.cy.js
@@ -0,0 +1,9 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://google.com')
+    cy.get('#W0wltc > .QS5gu').click()
+    cy.get('#APjFqb').type('Gmail')
+    //cy.contains('.FPdoLc > center > .gNO89b').click()
+    //This is my branch
+  })
+})
