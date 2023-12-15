91352c1d6354f3657f042775fa8a05708d2721b0
Pooja-Gangavathi <151650049+Pooja-Gangavathi@users.noreply.github.com>
2023-12-15 12:07:29 +0100

    Update Spec.cy.js

--- cypress/e2e/Spec.cy.js
+++ cypress/e2e/Spec.cy.js
@@ -3,7 +3,7 @@ describe('template spec', () => {
     cy.visit('https://google.com')
     cy.get('#W0wltc > .QS5gu').click()
     cy.get('#APjFqb').type('Gmail')
-    //cy.contains('.FPdoLc > center > .gNO89b').click()
+    cy.contains('.FPdoLc > center > .gNO89b').click()
     //This is my branch
   })
 })
